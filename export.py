e499abadbfb19e6b4ae70b464c9fe8fa28316524
DaWe <willteam35@gmail.com>
2019-04-21 19:30:14 +0200

    Remove string from brackets

--- export.py
+++ export.py
@@ -1,8 +1,9 @@
 # !/usr/bin/python
 
-file = 'C:\webserver\www\music_names\dir.txt'
+file = 'C:\webserver\www\music_names\Music list export.txt'
 folder = 'C:\Users\Hp\Music'
 
+import re
 import os
 string = ''
 open(file, 'w').close()
@@ -11,12 +12,14 @@ for root, dirs, files in os.walk(folder, topdown=True):
        file_ = os.path.join(name)
        filename, file_extension = os.path.splitext(file_)
        if (file_extension == '.mp3') or (file_extension == '.wav'):
-            splitted = filename.split('-', 1)
-            splitted[0] = splitted[0].strip()
-            if len(splitted) >= 2:
-                string += splitted[0] + '\t' + splitted[1] + '\n'
-            else:
-                string += splitted[0] + '\n'
+           filename = re.sub(r" ?\([^)]+\)", "", filename)
+           filename = re.sub(r" ?\[[^)]+\]", "", filename)
+           splitted = filename.split('-', 1)
+           splitted[0] = splitted[0].strip()
+           if len(splitted) >= 2:
+               string += splitted[0] + '\t' + splitted[1] + '\n'
+           else:
+               string += splitted[0] + '\n'
    '''for name in dirs:
       print(os.path.join(root, name))'''
 
