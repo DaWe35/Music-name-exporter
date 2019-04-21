a1427aea8dca78cfe152d0a47e77b29864d86eb4
DaWe <willteam35@gmail.com>
2019-04-21 10:25:03 +0200

    read works

--- export.py
+++ export.py
@@ -1,11 +1,8 @@
-path = 'C:\webserver\www\music_names\dir.txt'
-with open(path, 'r', errors='ignore') as current:
-    lines = current.readlines()
-    if not lines:
-        print('FILE IS EMPTY')
-    else:
-        for line in lines:
-            line = line.strip()
-            print(line)
-            if (line.endswith(('.mp3', '.avi'))):
-                print(line)
+filepath = 'C:\webserver\www\music_names\dir.txt'
+with open(filepath) as fp:
+   line = fp.readline()
+   cnt = 1
+   while line:
+       print("Line {}: {}".format(cnt, line.strip()))
+       line = fp.readline()
+       cnt += 1
