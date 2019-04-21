d8e3b60444608f725163dcf2c68bbf33fb503b51
DaWe <willteam35@gmail.com>
2019-04-21 15:15:31 +0200

    python export working

--- export.py
+++ export.py
@@ -4,32 +4,22 @@ file = 'C:\webserver\www\music_names\dir.txt'
 folder = 'C:\Users\Hp\Music'
 
 import os
+string = ''
+open(file, 'w').close()
 for root, dirs, files in os.walk(folder, topdown=True):
    for name in files:
-      print(os.path.join(root, name))
-   for name in dirs:
-      print(os.path.join(root, name))
-
-'''
-import os
-
-
-
-
-from os import walk
-
-f = []
-for (dirpath, dirnames, filenames) in walk(folder):
-    print(filenames)
-    break
+       file_ = os.path.join(name)
+       filename, file_extension = os.path.splitext(file_)
+       if (file_extension == '.mp3') or (file_extension == '.wav'):
+            splitted = filename.split('-', 1)
+            splitted[0] = splitted[0].strip()
+            if len(splitted) >= 2:
+                string += splitted[0] + '\t' + splitted[1] + '\n'
+            else:
+                string += splitted[0] + '\n'
+   '''for name in dirs:
+      print(os.path.join(root, name))'''
 
 
-
-
-with open(filepath) as fp:
-   line = fp.readline()
-   cnt = 1
-   while line:
-       print("Line {}: {}".format(cnt, line.strip()))
-       line = fp.readline()
-       cnt += 1'''
+new_days = open(file,'w')
+new_days.write(string)
