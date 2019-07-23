b4dce85408cac3d411056e58c6535f420cbadfe6
DaWe <10603822+DaWe35@users.noreply.github.com>
2019-07-23 16:33:03 +0200

    CollectAfter date - collect music names modified after *

--- export.py
+++ export.py
@@ -1,32 +1,42 @@
 # !/usr/bin/python
+import re
+import os
+import time
+from datetime import datetime
+
 
 file = 'C:\webserver\www\Music-name-exporter\Music list export.txt'
 folder = 'C:\Users\Hp\Music'
-
 exclude = ['C:\Users\Hp\Music\Samples']
+collectAfter = '2019-04-29'
 
-import re
-import os
+collectAfter = datetime.strptime(collectAfter, '%Y-%m-%d')
 file_array = []
 open(file, 'w').close()
 for root, dirs, files in os.walk(folder, topdown=True):
     for name in files:
-        file_ = os.path.join(name)
         notExc = True
         for exc in exclude:
             if root.startswith(exc):
                 notExc = False
         if notExc:
-            filename, file_extension = os.path.splitext(file_)
-            if (file_extension == '.mp3') or (file_extension == '.wav'):
-                filename = re.sub(r" ?\([^)]+\)", "", filename)
-                filename = re.sub(r" ?\[[^)]+\]", "", filename)
-                splitted = filename.split('-', 1)
-                splitted[0] = splitted[0].strip()
-                if len(splitted) >= 2:
-                    file_array.append(splitted[0] + '\t' + splitted[1].strip())
-                else:
-                    file_array.append(splitted[0])
+            file_ = os.path.join(root, name)
+            try:
+                last_modified = datetime.utcfromtimestamp(os.path.getmtime(file_))
+                if last_modified > collectAfter:
+                    filename, file_extension = os.path.splitext(name)
+                    print(filename)
+                    if (file_extension == '.mp3') or (file_extension == '.wav'):
+                        filename = re.sub(r" ?\([^)]+\)", "", filename)
+                        filename = re.sub(r" ?\[[^)]+\]", "", filename)
+                        splitted = filename.split('-', 1)
+                        splitted[0] = splitted[0].strip()
+                        if len(splitted) >= 2:
+                            file_array.append(splitted[0] + '\t' + splitted[1].strip())
+                        else:
+                            file_array.append(splitted[0])
+            except Exception as e:
+                print(e)
     '''for name in dirs:
         print(os.path.join(root, name))'''
 
