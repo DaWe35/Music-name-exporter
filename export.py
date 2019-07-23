06a43f1d2da7ffd411233b736c7b575503a111ee
DaWe <10603822+DaWe35@users.noreply.github.com>
2019-07-23 13:29:34 +0200

    exclude folders array

--- export.py
+++ export.py
@@ -1,27 +1,34 @@
 # !/usr/bin/python
 
-file = 'C:\webserver\www\music_names\Music list export.txt'
+file = 'C:\webserver\www\Music-name-exporter\Music list export.txt'
 folder = 'C:\Users\Hp\Music'
+
+exclude = ['C:\Users\Hp\Music\Samples']
 
 import re
 import os
 file_array = []
 open(file, 'w').close()
 for root, dirs, files in os.walk(folder, topdown=True):
-   for name in files:
-       file_ = os.path.join(name)
-       filename, file_extension = os.path.splitext(file_)
-       if (file_extension == '.mp3') or (file_extension == '.wav'):
-           filename = re.sub(r" ?\([^)]+\)", "", filename)
-           filename = re.sub(r" ?\[[^)]+\]", "", filename)
-           splitted = filename.split('-', 1)
-           splitted[0] = splitted[0].strip()
-           if len(splitted) >= 2:
-               file_array.append(splitted[0] + '\t' + splitted[1].strip())
-           else:
-               file_array.append(splitted[0])
-   '''for name in dirs:
-      print(os.path.join(root, name))'''
+    for name in files:
+        file_ = os.path.join(name)
+        notExc = True
+        for exc in exclude:
+            if root.startswith(exc):
+                notExc = False
+        if notExc:
+            filename, file_extension = os.path.splitext(file_)
+            if (file_extension == '.mp3') or (file_extension == '.wav'):
+                filename = re.sub(r" ?\([^)]+\)", "", filename)
+                filename = re.sub(r" ?\[[^)]+\]", "", filename)
+                splitted = filename.split('-', 1)
+                splitted[0] = splitted[0].strip()
+                if len(splitted) >= 2:
+                    file_array.append(splitted[0] + '\t' + splitted[1].strip())
+                else:
+                    file_array.append(splitted[0])
+    '''for name in dirs:
+        print(os.path.join(root, name))'''
 
 file_array.sort()
 #file_array = list(dict.fromkeys(file_array)) # Remove duplicates (comment out if needed)
